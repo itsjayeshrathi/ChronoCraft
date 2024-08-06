af83c54b6066ad4a7c5670ad441a2aed26bf2da6
Jayesh Rathi <itsjayeshrathi@gmail.com>
2024-08-06 18:06:26 +0530

    setup done

---
+++ server/src/main/java/com/chronocraft/ChronoCraft/ChronoCraftApplication.java
@@ -0,0 +1,13 @@
+package com.chronocraft.ChronoCraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChronoCraftApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/chronocraft/ChronoCraft/ChronoCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronocraft.ChronoCraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
