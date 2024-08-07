e6132f655d425afc4033aff4b7f166011d3c61aa
Jayesh Rathi <itsjayeshrathi@gmail.com>
2024-08-07 17:26:00 +0530

    worked on front end and created exception classes

---
+++ client/src/Components/Home/MainCarouselData.js
@@ -0,0 +1,6 @@
+export const data = [
+  { image: "", path: "" },
+  { image: "", path: "" },
+  { image: "", path: "" },
+  { image: "", path: "" },
+];
---
+++ client/src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import MainCarousel from "../Components/Home/MainCarousel"
+
+function HomePage() {
+    return (
+        <div>
+            <MainCarousel/>
+        </div>
+    )
+}
+
+export default HomePage
